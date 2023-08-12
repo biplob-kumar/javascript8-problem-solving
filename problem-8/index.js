e855cdeaed48110b0736d519ccdab08a37b59864
biplob kumar <biplobkumar125@gmail.com>
2023-08-12 16:27:44 +0600

    problem-8

---
+++ problem-8/index.js
@@ -0,0 +1,9 @@
+// Write a javaScript program that prints this pattern below.
+
+for (let i = 1; i <=5; i++) {
+
+   for (let j = 1; j<=i; j++) {
+   console.log("*");
+   }
+    console.log("<br/>");
+}
